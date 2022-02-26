09cf993d0f4f4756aa88a61693fd127e24ae2b7e
Pritom Rahaman <pritomrahaman1331@gmail.com>
2022-02-27 00:34:35 +0530

    change in navbar in toggle

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
                         <nav className="navbar navbar-expand-md navbar-light bg-light">
                             <div className="container-fluid">
                                 <NavLink className="navbar-brand" to="/"> Navbar</NavLink>
-                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarSupportedContent"
+                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
